36e89d2b7320b91b8d3a21fced5ac7ddeb25ebe4
shelkemayur4700 <shelkemayur4700@gmail.com>
2026-02-10 18:19:45 +0530

    Login button fixed

--- packages/ui/src/pages/login/components/LoginForm.tsx
+++ packages/ui/src/pages/login/components/LoginForm.tsx
@@ -25,15 +25,10 @@ export function LoginForm({
       <div className="grid gap-6">
         <Button 
           variant="outline" 
-          className="w-full h-12 text-base font-medium relative hover:bg-zinc-50 dark:hover:bg-zinc-800 transition-all border-zinc-200 dark:border-zinc-800" 
+          className="w-full mt-2 h-12 text-base font-medium relative hover:scale-105 dark:hover:bg-zinc-900 transition ease-in-out duration-300 dark:bg-zinc-800 transition-all border-zinc-200 dark:border-zinc-800"  
           onClick={handleGoogleLogin}
           disabled={isLoading}
         >
-          <div className="absolute left-4">
-            <svg className="h-5 w-5" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-              <path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
-            </svg>
-          </div>
           Login with Google
         </Button>
       </div>
